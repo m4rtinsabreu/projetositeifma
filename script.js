aa044f12d37e2fe36b81ee6155835699ef3e1797
m4rtinsabreu <erickabreumartins2000@gmail.com>
2024-10-14 19:52:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mapa = document.getElementById('mapa');
+    mapa.innerHTML = '<p>https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.74534291125!2d-43.02872128983486!3d-4.2696646956863225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x78daf5bedaf2463%3A0xc041d66f38b98c30!2sIFMA%20-%20Campus%20Coelho%20Neto!5e0!3m2!1spt-BR!2sbr!4v1728863945754!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"</p>';
+   
+});
